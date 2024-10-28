661b49aa0a99ab69869147446b9e74327adc70d6
mont1cc <141335185+StepanKud@users.noreply.github.com>
2024-10-28 12:06:49 +0400

    Create LCM.cpp

---
+++ LCM.cpp
@@ -0,0 +1,34 @@
+#include<algorithm> 
+#include<iostream>  
+#include<iomanip>   
+#include<numeric>   
+#include<string>   
+#include<vector>   
+#include<deque>   
+#include<cmath>   
+#include<array>   
+#include<queue>   
+#include<set>   
+#include<map>   
+
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b) {
+	ll ma = max(a, b), mi = min(a, b);
+	if (mi == 0) {
+		return ma;
+	}
+	return gcd(mi, ma % mi);
+}
+
+ll lcm(ll a, ll b) {
+	return (a * b) / gcd(a, b);
+}
+
+int main(){
+	ll a, b;
+	cin >> a >> b;
+	cout << lcm(a, b);
+}
